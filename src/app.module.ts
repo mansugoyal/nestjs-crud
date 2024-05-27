a085277d71d3170de78196cbef74dceca1da927d
mansugoyal <mansugoyal1234@gmail.com>
2024-05-27 14:28:59 +0530

    crud wokring fine

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './module/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TodosModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'redhat@123',
+      database: 'nestjscrud',
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
