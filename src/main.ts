fc53f3d4285e381540cd852a5f71d7265bba8966
mansugoyal <mansugoyal1234@gmail.com>
2024-05-27 12:36:44 +0530

    added crud files

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { NestFactory } from '@nestjs/core'; 
-import { AppModule } from './app.module'; 
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 declare const module: any;
 
 
 
-async function bootstrap() { 
-  const app = await NestFactory.create(AppModule, { cors: true }); 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.listen(5000)
-  .then(() => { 
-    console.log("successfully stared on port 5000"); 
-  }).catch((error) => { 
-    console.log(error); 
-  }) 
+    .then(() => {
+      console.log("successfully stared on port 5000");
+    }).catch((error) => {
+      console.log(error);
+    })
 } bootstrap();
