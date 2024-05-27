9e4a3070a38d9cc3e6a38e31e2e16afe1d7a85c2
mansugoyal <mansugoyal234@gmail.com>
2024-05-27 12:35:01 +0530

    added crud files

---
+++ src/config/database.config.ts
@@ -0,0 +1,12 @@
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+ 
+export const devConfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'redhat@123',
+  database: 'nestjscrud ',
+  entities: ["dist/**/*.entity{.ts,.js}"],
+  synchronize: true,
+}
